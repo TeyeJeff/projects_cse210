650f5cd8fa00b28563a38db37fc49f86f9196928
TeyeJeff <teyejeff18@gmail.com>
2025-05-13 21:40:49 +0000

    Exercise 4 Assignmnet

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,30 +7,33 @@ class Program
     {
         List<int> numbers = new List<int>();
         
+    
         int userNumber = -1;
         while (userNumber != 0)
         {
-            Console.Write("Enter your of number, (type 0 to quit): ");
+            Console.Write("Please enter your number (Enter 0 to quit): ");
             
             string userResponse = Console.ReadLine();
             userNumber = int.Parse(userResponse);
-
+            
+            
             if (userNumber != 0)
             {
                 numbers.Add(userNumber);
             }
         }
-        
+
         int sum = 0;
         foreach (int number in numbers)
         {
             sum += number;
         }
 
-        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The sum is: {sum}");
+
 
         float average = ((float)sum) / numbers.Count;
-        Console.Writeline($"The average is: {average}");
+        Console.WriteLine($"The average is: {average}");
         
         int max = numbers[0];
 
@@ -42,6 +45,6 @@ class Program
             }
         }
 
-        Console.WriteLine($"The max is {max}");
+        Console.WriteLine($"The maximum number is: {max}");
     }
 }
